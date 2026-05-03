9c4d395fbc0a48ddf52d3a453ffdd20c2c7515b0
Saud khan <soudkhan82@gmail.com>
2026-05-04 01:19:08 +0500

    homepage with authentication

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,5 +3,5 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const { origin } = new URL(request.url);
 
-  return NextResponse.redirect(`${origin}/onboarding`);
+  return NextResponse.redirect(`${origin}/`);
 }
